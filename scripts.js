3e37571c1fcada9c5735a9b8c1fc5e16f59c7db2
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2023-10-13 20:12:46 +0200

    added new component grid and the bootstrap grid

--- scripts.js
+++ scripts.js
@@ -1,15 +1,34 @@
 function loadContent(type) {
-    let frame1 = document.getElementById('frame1');
-    let frame2 = document.getElementById('frame2');
-    let frame3 = document.getElementById('frame3');
+    let columnSection = document.querySelector('.column-section');
+    let rowSection = document.querySelector('.row-section');
+
+    let columnFrame1 = document.getElementById('columnFrame1');
+    let columnFrame2 = document.getElementById('columnFrame2');
+    let columnFrame3 = document.getElementById('columnFrame3');
+    
+    let rowFrame1 = document.getElementById('rowFrame1');
+    let rowFrame2 = document.getElementById('rowFrame2');
+    let rowFrame3 = document.getElementById('rowFrame3');
 
     if (type === 'card') {
-        frame1.src = 'bootstrap/bootstrap-card.html';
-        frame2.src = 'tailwind/tailwind-card.html';
-        frame3.src = 'foundation/foundation-card.html';
+        columnFrame1.src = 'bootstrap/bootstrap-card.html';
+        columnFrame2.src = 'tailwind/tailwind-card.html';
+        columnFrame3.src = 'foundation/foundation-card.html';
+        columnSection.style.display = 'block';
+        rowSection.style.display = 'none';
     } else if (type === 'buttons') {
-        frame1.src = 'bootstrap/bootstrap-buttons.html';
-        frame2.src = 'tailwind/tailwind-buttons.html';
-        frame3.src = 'foundation/foundation-buttons.html';
+        columnFrame1.src = 'bootstrap/bootstrap-buttons.html';
+        columnFrame2.src = 'tailwind/tailwind-buttons.html';
+        columnFrame3.src = 'foundation/foundation-buttons.html';
+
+        columnSection.style.display = 'block';
+        rowSection.style.display = 'none';
+    } else if (type === 'grid') {
+        rowFrame1.src = 'bootstrap/bootstrap-grid.html'
+        rowFrame2.src = 'tailwind/tailwind-grid.html'
+        rowFrame3.src = 'foundation/foundation-grid.html'
+
+        rowSection.style.display = 'block';
+        columnSection.style.display = 'none';
     }
 }
